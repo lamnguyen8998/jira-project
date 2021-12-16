a13164a4ba65e1e423ba125b7468ed2d4a545e61
lamnguyen8998 <nguyenlam2448@gmail.com>
2021-12-17 01:02:23 +0700

    update

--- react-jira/src/containers/HomeTemplate/Management/TableProjects/index.js
+++ react-jira/src/containers/HomeTemplate/Management/TableProjects/index.js
@@ -79,6 +79,11 @@ export default class TableProjects extends Component {
                                                                 <td className="p-2 whitespace-nowrap">
                                                                     <div className="text-left font-medium text-green-500">Lâm Nguyễn</div>
                                                                 </td>
+                                                                <div className="w-4 mr-2 py-4 transform hover:text-purple-500 hover:scale-110">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                                    </svg>
+                                                                </div>
 
                                                             </tr>
                                                         </tbody>
